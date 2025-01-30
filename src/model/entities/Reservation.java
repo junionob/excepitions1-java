aec88444894d6cd59f55f3a13c6668d6d827c8ae
junionob <junio.waldomiro@gmail.com>
2025-01-30 10:43:26 -0300

    criando classe Reservation

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+
+    private static SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy" );
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Long duration() {
+        long diff = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkin, Date checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    @Override
+    public String toString() {
+        return "Room " + roomNumber +
+                ", check-in: " + formatDate.format(checkin)  +
+                ", check-out: " + formatDate.format(checkout) +
+                ", " + duration() + "nights" ;
+    }
+}
